13993770880bf38fdf016e38c61dc93bdd5cbbc6
Petter-Nam <150123411+Petter-Nam@users.noreply.github.com>
2024-06-19 12:18:00 +0900

    테스트

---
+++ ModuDeal/src/main/java/com/application/moduDeal/qna/QnaController.java
@@ -0,0 +1,5 @@
+package com.application.moduDeal.qna;
+
+public class QnaController {
+
+}
--- ModuDeal/src/test/java/com/application/moduDeal/ModuDealApplicationTests.java
+++ ModuDeal/src/test/java/com/application/moduDeal/ModuDealApplicationTests.java
@@ -6,8 +6,11 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class ModuDealApplicationTests {
 
+	/*
 	@Test
 	void contextLoads() {
 	}
+	*/
+	
 
 }
