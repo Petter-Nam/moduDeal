e1b36937b594949212f97dfe3557f3ecde9d0543
Petter-Nam <150123411+Petter-Nam@users.noreply.github.com>
2024-06-19 13:28:14 +0900

    aa

--- ModuDeal/src/main/java/com/application/moduDeal/ModuDealMainController.java
+++ ModuDeal/src/main/java/com/application/moduDeal/ModuDealMainController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class ModuDealMainController {
 
-    @GetMapping()
+    @GetMapping
     public String main() {
         return "moduDeal/main.html";
     }
