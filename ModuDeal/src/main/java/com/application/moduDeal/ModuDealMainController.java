3bfd13477b958b731a290866a0b75c918e3b6ef7
Petter-Nam <150123411+Petter-Nam@users.noreply.github.com>
2024-06-20 14:38:56 +0900

    dsaf

--- ModuDeal/src/main/java/com/application/moduDeal/ModuDealMainController.java
+++ ModuDeal/src/main/java/com/application/moduDeal/ModuDealMainController.java
@@ -22,6 +22,6 @@ public class ModuDealMainController {
 
         model.addAttribute("recentProducts", recentProducts);
 
-        return "/moduDeal/main";
+        return "moduDeal/main";
     }
 }
